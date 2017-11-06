e792e480a80023347392742616b17cb697ef0670
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-11-06 17:42:40 +0000

    Fix #6

--- coolsola.js
+++ coolsola.js
@@ -1,15 +1,63 @@
+
 var coolsola = {};
+
 
 // Config
 coolsola.setup = {
-        mute: false
+        mute: false,
+        HTMLMode: false
     };
+
+coolsola.resources = {
+    messages: []
+}
+
+coolsola.logger = function(){
+    var element = document.getElementById("coolsola")
+    if(element){
+        var htmlContent = ""
+        
+        coolsola.resources.messages.forEach(function(item){
+            htmlContent += "<p>"+item+"</p>"
+        })
+        
+        element.innerHTML = htmlContent;
+    }
     
+    
+}
+
 // Métodos
 coolsola.msg = function(data){
+        // Almacenar el dato
+        coolsola.resources.messages.push(data)
+    
+        // Mostrar al usuario
         if(!coolsola.setup.mute){
             console.log(data)
         }
+        
+        // Actualizar logger
+        if(coolsola.setup.HTMLMode){
+            coolsola.logger()
+        }
+        
     }
+    
+coolsola.msg("Coolsola - v.0.2.0: Cargado....");
 
-console.log("Coolsola - v.0.2.0: Cargado....");
+  document.addEventListener("DOMContentLoaded", function(event) {
+     if(coolsola.setup.HTMLMode){
+        document.body.innerHTML += "<h3>Coolsola Logger!</h3><div id='coolsola'></div>"
+        console.log(document.body)
+        coolsola.msg("Coolsola - v.0.2.0: Cargado en HTML....");
+     }
+  });
+
+
+
+    
+
+
+
+
