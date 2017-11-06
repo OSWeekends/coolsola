94c9d728ef9d643b6558b90cf9d4d0a959782af8
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-11-06 17:06:28 +0000

    funcionalidad mute. Fix #1

--- coolsola.js
+++ coolsola.js
@@ -1,7 +1,24 @@
-var coolsola = {
-    setup: {},
-    msg: function(data){
-        console.log(data)
-    }
-};
+var coolsola = {};
 
+// Config
+coolsola.setup = {
+        mute: false
+    };
+    
+// Métodos
+coolsola.msg = function(data){
+        if(!coolsola.setup.mute){
+            console.log(data)
+        }
+    }
+
+
+coolsola.msg("NO Estoy en Mute....")
+
+coolsola.setup.mute = true;
+
+coolsola.msg("Estoy en Mute....")
+
+coolsola.setup.mute = false;
+
+coolsola.msg("NO Estoy en Mute....")
